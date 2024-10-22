5957aaf0d98a02ed8fe7abb646525bce5da5c846
Guillaume Simonet <guillaume@krafter.io>
2024-10-22 09:39:36 +0200

    ADD functions CRUD to Model

--- ORM/model.py
+++ ORM/model.py
@@ -5,13 +5,14 @@ class Model:
     table_name = ''
 
     def create(self):
+
         if self.table_name == 'app_user':
             if 'id' in self.__dict__:
-                del self.__dict__['id']  # Supprimer l'ID pour l'insertion
+                del self.__dict__['id']
             if 'created_at' in self.__dict__:
-                del self.__dict__['created_at']  # Supprimer l'ID pour l'insertionfame_rate
+                del self.__dict__['created_at']
             if 'fame_rate' in self.__dict__:
-                del self.__dict__['fame_rate']  # Supprimer l'ID pour l'insertion
+                del self.__dict__['fame_rate']
 
         columns = ', '.join(self.__dict__.keys())
         placeholders = ', '.join(['%s'] * len(self.__dict__))
@@ -19,11 +20,21 @@ class Model:
         values = tuple(self.__dict__.values())
         return db.execute(query, values)
 
-    # def delete(self):
-    #     columns = ', '.join(self.__dict__.keys())
-    #
-    # def all(self):
-    #     query = f"SELECT * FROM {self.table_name}"
-    #
-    # def update(self):
-    #     columns = ', '.join(self.__dict__.keys())
+    def delete(self):
+        query = f"DELETE FROM {self.table_name} WHERE id = %s;"
+        values = tuple(self.__dict__['id'])
+        return db.execute(query, values)
+
+    def find_by_id(self, id):
+        query = f"SELECT * FROM {self.table_name} WHERE id = %s;"
+        return db.execute(query, (id,))
+
+    def all(self):
+        query = f"SELECT * FROM {self.table_name}"
+        return db.execute(query)
+
+    def update(self):
+        set_clause = ', '.join([f"{key} = %s" for key in self.__dict__.keys() if key != 'id'])
+        query = f"UPDATE {self.table_name} SET {set_clause} WHERE id = %s;"
+        values = tuple([v for k, v in self.__dict__.items() if k != 'id']) + (self.__dict__.get('id'),)
+        return db.execute(query, values)
