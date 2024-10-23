2d207764e0cfc1f65203ac7783db86e52ee38ea5
Guillaume Simonet <guillaume@krafter.io>
2024-10-23 14:29:57 +0200

    ADD delete_mass and update_mass

--- ORM/model.py
+++ ORM/model.py
@@ -1,4 +1,3 @@
-from tkinter import BooleanVar
 from typing import Any
 from werkzeug.exceptions import NotFound
 from ORM.database import db
@@ -11,10 +10,10 @@ class Model:
     # ------------------------------------ CREATE
     def create(self):
         if self.table_name == 'app_user':
-            if 'id' in self.__dict__:
-                del self.__dict__['id']
-            if 'created_at' in self.__dict__:
-                del self.__dict__['created_at']
+            # if 'id' in self.__dict__:
+            #     del self.__dict__['id']
+            # if 'created_at' in self.__dict__:
+            #     del self.__dict__['created_at']
             if 'fame_rate' in self.__dict__:
                 del self.__dict__['fame_rate']
         attrs = {key: value for key, value in self.__dict__.items() if key not in ['id', 'created_at']}
@@ -83,14 +82,30 @@ class Model:
         set_clause = ', '.join([f"{key} = %s" for key in changes.keys() if key != 'id'])
         query = f"UPDATE {self.table_name} SET {set_clause} WHERE id = %s;"
         values = tuple([v for k, v in changes.items() if k != 'id']) + (self.id,)
-        print(query)
-        print(values)
+        db.execute(query, values, False)
+        return True
+    
+    @classmethod
+    def update_mass(self, changes:dict[str, Any], ids: [int]) -> bool:
+        for k in changes.keys():
+            if k not in self.column_names:
+                raise NotFound(f'Column name {k} is not in columns of {self.table_name}')
+        set_clause = ', '.join([f"{key} = %s" for key in changes.keys() if key != 'id'])
+        query = f"UPDATE {self.table_name} SET {set_clause} WHERE id IN ({', '.join(str(id) for id in ids)});"
+        values = tuple([v for k, v in changes.items() if k != 'id'])
+        print('values = ', values)
         db.execute(query, values, False)
         return True
 
     # ------------------------------------ DELETE
     def delete(self):
-        print('DELETE self.table_name = ', self.id)
         query = f"DELETE FROM {self.table_name} WHERE id = %s;"
         db.execute(query, (self.id,), False)
+        return True
+    
+    @classmethod
+    def delete_mass(cls, ids: [int]):
+        query = f"DELETE FROM {cls.table_name} WHERE id IN ({', '.join(str(id) for id in ids)});"
+        print(query)
+        db.execute(query, fetch=False)
         return True
