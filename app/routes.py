a1047c4a66bd8638a74a39ebfacd958e7817c4a8
Guillaume Simonet <guillaume@krafter.io>
2024-10-23 17:23:49 +0200

    Use flask session, redirect, url_for, and request

--- app/routes.py
+++ app/routes.py
@@ -1,29 +1,72 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session, redirect, url_for, request, abort
 
 
 main = Blueprint('main', __name__)
 
 
+@main.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if username == 'admin' and password == 'psd':  # Remplacez par votre logique
+            session['username'] = username
+            return redirect(url_for('main.home'))
+
+        return 'Identifiants incorrects', 401  # Message d'erreur en cas d'échec
+
+    if request.method == 'GET':
+        if 'username' in session:
+            return render_template('search.html')
+        return render_template('login.html')
+
+@main.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('main.login'))
+
+@main.route('/register')
+def register():
+    return render_template('register.html')
+
 @main.route('/')
 def home():
-    return render_template('search.html')
-
-@main.route('/chat')
-def chat():
-    return render_template('chat.html')
+    if 'username' in session:
+        return render_template('search.html')
+    return redirect(url_for('main.login'))
 
 @main.route('/historic')
 def historic():
-    return render_template('historic.html')
+    if 'username' in session:
+        return render_template('historic.html')
+    return redirect(url_for('main.login'))
 
 @main.route('/notifs')
 def notifs():
-    return render_template('notifs.html')
+    if 'username' in session:
+        return render_template('notifs.html')
+    return redirect(url_for('main.login'))
+
+@main.route('/chat')
+def chat():
+    if 'username' in session:
+        return render_template('chat.html')
+    return redirect(url_for('main.login'))
+
 
 @main.route('/profile')
 def profile():
-    return render_template('profile.html')
+    if 'username' in session:
+        return render_template('profile.html')
+    return redirect(url_for('main.login'))
 
 @main.route('/user')
 def user():
-    return render_template('user.html')
+    if 'username' in session:
+        return render_template('user.html')
+    return redirect(url_for('main.login'))
+
+@main.app_errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
