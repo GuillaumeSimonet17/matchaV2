0c74e29314a60a01d171cd804a0f7f90bb4cc194
Guillaume Simonet <guillaume@krafter.io>
2024-10-27 16:51:28 +0100

    Move def go_profile to profile_management and create Visit if not exist

---
+++ app/managements/profile_management/profile_management.py
@@ -0,0 +1,42 @@
+from flask import Blueprint, flash, request, render_template, session, redirect, url_for
+
+from ORM.views.profile import Profile
+from ORM.tables.friendship import Friendship
+from ORM.tables.user import User
+from ORM.tables.tag import UserTag, Tag
+from ORM.tables.visit import Visit
+
+def go_profile(profile_id: int):
+
+    # TODO : opti cette merde
+    user_id = session['user_id']
+    if isinstance(user_id, tuple):
+        user_id = session['user_id'][0]
+    
+    profile = Profile._find_by_id(profile_id)
+    profile_image_data = User.get_profile_image(profile.id)
+    user_tag_ids = UserTag.find_tags_by_user_id(profile.id)
+    
+    user_tags = []
+    for tag in user_tag_ids:
+        user_tags.append(Tag._find_by_id(tag.id))
+    
+    friendship = Friendship.get_friendship_by_user_ids([user_id, profile_id])
+    state, connected, recevied_invitation, sent_invitation = False, False, False, False
+    if friendship:
+        state = friendship.state
+        if state != 'connected':
+            recevied_invitation = friendship.receiver_id == session['user_id']
+            sent_invitation = friendship.sender_id == session['user_id']
+        else:
+            connected = True
+    
+    if not (Visit.find_visit(user_id, profile_id)):
+        visit = Visit(None, user_id, profile_id)
+        visit.create()
+
+    # send a notif to profile_id
+    
+    return render_template('profile.html', profile=profile, state=state, connected=connected,
+                           profile_image_data=profile_image_data, recevied_invitation=recevied_invitation,
+                           sent_invitation=sent_invitation, user_tags=user_tags)
--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,13 @@
 from flask import Blueprint, flash, request, render_template, session, redirect, url_for
 
-from user_management.auth.login import auth_login
-from user_management.auth.register import auth_register
-from user_management.update_user import update_user_infos, change_password
+from managements.user_management.auth.login import auth_login
+from managements.user_management.auth.register import auth_register
+from managements.user_management.update_user import update_user_infos, change_password
+from managements.profile_management.profile_management import go_profile
 
 from ORM.tables.user import User
 from ORM.tables.tag import UserTag, Tag
 from ORM.views.profile import Profile
-from ORM.tables.friendship import Friendship
 from ORM.tables.notif import Notif
 from ORM.tables.visit import Visit
 
@@ -37,8 +37,6 @@ def register():
     tags = Tag._all()
     return render_template('register.html', tags=tags)
 
-
-import base64
 
 @main.route('/')
 def home():
@@ -113,32 +111,7 @@ def chat():
 @main.route('/profile/<int:profile_id>')
 def profile(profile_id):
     if 'username' in session:
-        user_id = session['user_id']
-        if isinstance(user_id, tuple):
-            user_id = session['user_id'][0]
-
-        profile = Profile._find_by_id(profile_id)
-        profile_image_data = User.get_profile_image(profile.id)
-        user_tag_ids = UserTag.find_tags_by_user_id(profile.id)
-
-        user_tags = []
-        for tag in user_tag_ids:
-            user_tags.append(Tag._find_by_id(tag.id))
-
-        friendship = Friendship.get_friendship_by_user_ids([user_id, profile_id])
-        state, connected, recevied_invitation, sent_invitation = False, False, False, False
-        if friendship:
-            state = friendship.state
-            if state != 'connected':
-                recevied_invitation = friendship.receiver_id == session['user_id']
-                sent_invitation = friendship.sender_id == session['user_id']
-            else:
-                connected = True
-
-        # add historic tp user_id in db AND a notif to profile_id
-        return render_template('profile.html', profile=profile, state=state, connected=connected,
-                               profile_image_data=profile_image_data, recevied_invitation=recevied_invitation,
-                               sent_invitation=sent_invitation, user_tags=user_tags)
+        return go_profile(profile_id)
     return redirect(url_for('main.login'))
 
 @main.route('/user', methods=['GET', 'POST'])
