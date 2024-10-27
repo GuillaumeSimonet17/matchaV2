52e64e53d223ef13a825d1680622cc372dd92c48
Guillaume Simonet <guillaume@krafter.io>
2024-10-27 18:08:35 +0100

    Comments for navbar management

--- app/managements/navbar_management/navbar_management.py
+++ app/managements/navbar_management/navbar_management.py
@@ -1,3 +1,11 @@
 
 # Gestion de la Navbar
 # écoute des events pour chat et notifs
+
+# j'aimerai que lorsqu'on arrive sur n'importe quelle route on appelle une fonction qui récupère :
+# --> les notifs (+chat),
+# --> block (plus tard),
+# --> les changements de friendship :
+    # get_invitations_received
+    # get_uninvitations_received
+    # get_friendship_connections
