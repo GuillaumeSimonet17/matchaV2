c69c2642bc5328b5e275d96fe6dbdc51d8bdb4db
Guillaume Simonet <guillaume@krafter.io>
2024-11-25 17:03:01 +0100

    [RMV] Remove socket_management.js file cause i moved the content into base.html

--- app/static/js/socket_management.js
+++ app/static/js/socket_management.js
@@ -1,21 +1,3 @@
-const socket = io();
-
-const currentUserIdElement = document.getElementById('current-user');
-const currentUserId = currentUserIdElement ? currentUserIdElement.getAttribute('data-current-user-id') : null;  // Utiliser l'attribut 'data-id'
-
-// Émettre un événement de jointure
-if (currentUserId) {
-    socket.emit('join', { user_id: currentUserId });
-}
-
-// Écouter l'événement 'current_user'
-socket.on('current_user', function(data) {
-    console.log('current_user => ', data);
-});
-// ----------------------------------- ON -------------------------------------
-
-socket.on('current_user', function(data) {
-    console.log('current_user => ', data);
-});
-
-export default socket;
+// const socket = io();
+//
+// export default socket;
