5a542cd508d729e64c0cea23e11490f9d31dd701
Roymain <romain.cu69@gmail.com>
2025-03-15 15:29:41 +0100

    #test test good

--- init-db.sh
+++ init-db.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    \i /docker-entrypoint-initdb.d/dump.sql
-EOSQL 
+echo "Initialisation de la base de données dans le conteneur..."
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -f /docker-entrypoint-initdb.d/init.sql
+
+echo "Vérification des tables créées..."
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "\dt"
+
+echo "Initialisation terminée." 
